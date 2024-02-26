5268bae9f50a86bc054f442a3c30e23821dc2ee8
Marzena Skórska <maczenta@gmail.com>
2024-02-26 20:50:08 +0000

    Add about_ me function in views.py in about app

--- my_dog_project/urls.py
+++ my_dog_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from about.views import about_me
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('about/', about_me, name='about'),
+    path('', admin.site.urls),
 ]
