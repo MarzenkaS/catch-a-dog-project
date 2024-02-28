68845dba312b7cae72d7df16266f95a544d6191d
Marzena Skórska <maczenta@gmail.com>
2024-02-28 16:11:43 +0000

    Create Reviews Model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Reviews(models.Model):
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="reviews")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='reviews_like')
+
+    def number_of_likes(self):
+        return self.likes.count()
+    
