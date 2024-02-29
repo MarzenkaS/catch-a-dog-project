ef4fa483104d2c3f6d5235aba5fef3ad390edf62
Marzena Skórska <maczenta@gmail.com>
2024-02-29 19:11:06 +0000

    Edit admin.py in reviews app

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Reviews
+from .models import Reviews, Comment
 
 # Register your models here.
 admin.site.register(Reviews)
+admin.site.register(Comment)
