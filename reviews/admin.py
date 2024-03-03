610ec91052714e2ca2216093ede742821df37d1d
Marzena Skórska <maczenta@gmail.com>
2024-03-03 22:11:39 +0000

    Edit admin.py in reviews app

--- reviews/admin.py
+++ reviews/admin.py
@@ -6,9 +6,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Reviews)
 class ReviewsAdmin(SummernoteModelAdmin):
 
-    search_fields = ['date', 'author']
-    list_filter = ('status', 'created_on')
+    search_fields = ['date', 'author', 'content']
     summernote_fields = ('content',)
+    list_display = ('author', 'status', 'created_on')
+    list_filter = ('status', 'created_on')
+
 
 
 # Register your models here.
