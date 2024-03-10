29a17c8e9218d4d96ff3f586f7ec898be2a2179d
Marzena Skórska <maczenta@gmail.com>
2024-03-10 21:24:59 +0000

    Add navbar toggler

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewsList.as_view(), name='home'),
-    path('reviews_detail', views.reviews_detail, name='reviews_detail'),
+    path('reviews_detail/', views.reviews_detail, name='reviews_detail'),
 ]
