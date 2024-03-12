ed7dc2a179bb1190e143b17a7273f78b4f5824b3
Marzena Skórska <maczenta@gmail.com>
2024-03-12 11:29:08 +0000

    Add  POST request from the comment form

--- reviews/views.py
+++ reviews/views.py
@@ -18,9 +18,18 @@ def reviews_detail(request, pk):
     """
     review = get_object_or_404(Reviews, id=pk)
     comments = review.comments.all().order_by("-created_on")
-    comment_count = review.comments.filter(approved=True).count()
-    comment_form = CommentForm()
     review_likes = review.likes.count()
+    comment_count = review.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+    comment_form = CommentForm()
 
     return render(request, "reviews/reviews_detail.html",
         {
