10440a1579946d0d45db8ba504405c4f1d7bd96d
Marzena Skórska <maczenta@gmail.com>
2024-07-23 19:02:27 +0000

    fix editing review-edit reviews_detail and reviews_detail template

--- reviews/views.py
+++ reviews/views.py
@@ -17,38 +17,35 @@ class ReviewsList(generic.ListView):
 def reviews_detail(request, pk):
     """
     Display an individual review
-
     """
     review = get_object_or_404(Reviews, id=pk)
     comments = review.comments.all().order_by("-created_on")
-    comment_count = review.comments.filter(approved=True).count()
-    if request.method == "POST":
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.author = request.user
-            comment.reviews = review
-            comment.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Comment submitted!'
-            )
+    comment_count = review.comments.count()  # Count all comments, regardless of approval status
 
+    # Initialize forms
     comment_form = CommentForm()
+    review_form = ReviewForm()
 
     if request.method == "POST":
-        review_form = ReviewForm(data=request.POST)
-        if review_form.is_valid():
-            review = review_form.save(commit=False)
-            review.author = request.user
-            review.reviews = review
-            review.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Review submitted'
-            )
-
-    review_form = ReviewForm()
+        if 'edit_review' in request.POST:  # Handle review update
+            review_form = ReviewForm(data=request.POST, instance=review)
+            if review_form.is_valid() and review.author == request.user:
+                updated_review = review_form.save(commit=False)
+                updated_review.approved = False
+                updated_review.save()
+                messages.success(request, 'Review updated and awaiting approval')
+                return HttpResponseRedirect(reverse('reviews_detail', args=[pk]))
+            else:
+                messages.error(request, 'Error updating review!')
+        else:  # Handle comment submission
+            comment_form = CommentForm(data=request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.author = request.user
+                comment.reviews = review
+                comment.save()
+                messages.success(request, 'Comment submitted!')
+                return HttpResponseRedirect(reverse('reviews_detail', args=[pk]))
 
     return render(request, "reviews/reviews_detail.html",
                   {
@@ -124,24 +121,22 @@ def add_review(request):
 
 def review_edit(request, review_id):
     """
-    view to edit reviews
+    View to edit a review
     """
     review = get_object_or_404(Reviews, pk=review_id)
 
     if request.method == "POST":
         review_form = ReviewForm(data=request.POST, instance=review)
-
-        if review_form.is_valid() and review.reviewer == request.user:
+        if review_form.is_valid() and review.author == request.user:
             review = review_form.save(commit=False)
-            review.reviews = review
             review.approved = False
             review.save()
-            messages.success(request, 'Review Updated!')
-            return HttpResponseRedirect(reverse('reviews_detail', args=[pk]))
+            messages.success(request, 'Review updated and awaiting approval')
+            return HttpResponseRedirect(reverse('reviews_detail', args=[review.pk]))
         else:
             messages.error(request, 'Error updating review!')
     else:
-         review_form = ReviewForm(instance=review)
+        review_form = ReviewForm(instance=review)
 
     return render(request, 'review_edit.html', {'review_form': review_form})
 
